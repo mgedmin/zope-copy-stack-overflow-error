9e5c89922fcaa04991fa979130d71310675ea536
Marius Gedminas <marius@gedmin.as>
2022-11-30 11:49:23 +0200

    Drop the overengineered argparse logic

--- repro.py
+++ repro.py
@@ -1,20 +1,16 @@
-import argparse
-
 import zope.copy
 from zope.container.btree import BTreeContainer
 
 
-def main():
-    parser = argparse.ArgumentParser(description="reproduce a zope.copy bug")
-    parser.add_argument("n", type=int, nargs='?', default=8000,
-                        help="number of items (default: {default})")
-    args = parser.parse_args()
+N_ITEMS = 8000
+
+
+def test_clone_large_btree_container():
     obj = BTreeContainer()
-    for n in range(args.n):
+    for n in range(N_ITEMS):
         obj[str(n)] = None
     zope.copy.clone(obj)
-    print(f"{len(obj)} items: success!")
 
 
 if __name__ == "__main__":
-    main()
+    test_clone_large_btree_container()
